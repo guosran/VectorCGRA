8ec94bbe6694fdcb614a7ff857f708cf3f889ed1
guosran <165251838+guosran@users.noreply.github.com>
2026-07-24 11:42:03 +0800

    Split routing register writes from FU operands

--- tile/TileRTL.py
+++ tile/TileRTL.py
@@ -213,22 +213,30 @@ class TileRTL(Component):
     # the FUs (via `fu_crossbar`) with the outports of the
     # `routing_crossbar` through the corresponding channels.
     for i in range(num_tile_outports):
-      s.fu_crossbar.send_data[i] //= s.tile_out_or_link[i].recv_fu
-      s.routing_crossbar.send_data[i] //= s.tile_out_or_link[i].recv_xbar
-      s.tile_out_or_link[i].send //= s.send_data[i]
+      s.tile_out_or_link[i].recv_fu.msg //= s.fu_crossbar.send_data[i].msg
+      s.tile_out_or_link[i].recv_fu.val //= s.fu_crossbar.send_data[i].val
+      s.tile_out_or_link[i].recv_xbar.msg //= \
+          s.routing_crossbar.send_data[i].msg
+      s.tile_out_or_link[i].recv_xbar.val //= \
+          s.routing_crossbar.send_data[i].val
+      s.send_data[i].msg //= s.tile_out_or_link[i].send.msg
+      s.send_data[i].val //= s.tile_out_or_link[i].send.val
+      s.tile_out_or_link[i].send.rdy //= s.send_data[i].rdy
 
     # Crossbars outputs are integrated with the "register_cluster".
     # Whether the required operands for FU are from the "routing_crossbar"
     # or from the "register_cluster" depends on the control signals.
     for i in range(num_fu_inports):
-      s.routing_crossbar.send_data[num_tile_outports + i] //= \
-          s.register_cluster.recv_data_from_routing_crossbar[i]
       s.register_cluster.write_data_from_routing_crossbar[i] //= \
           s.routing_crossbar.send_data[num_tile_outports + i].msg
       s.register_cluster.write_valid_from_routing_crossbar[i] //= \
           s.routing_write_fire[i]
-      s.fu_crossbar.send_data[num_tile_outports + i] //= \
-          s.register_cluster.recv_data_from_fu_crossbar[i]
+      s.register_cluster.recv_data_from_routing_crossbar[i].msg //= \
+          s.routing_crossbar.send_data[num_tile_outports + i].msg
+      s.register_cluster.recv_data_from_fu_crossbar[i].msg //= \
+          s.fu_crossbar.send_data[num_tile_outports + i].msg
+      s.register_cluster.recv_data_from_fu_crossbar[i].val //= \
+          s.fu_crossbar.send_data[num_tile_outports + i].val
 
       s.register_cluster.recv_data_from_const[i].msg //= DataType()
       s.register_cluster.recv_data_from_const[i].val //= 0
@@ -239,13 +247,31 @@ class TileRTL(Component):
 
 
     @update
-    def update_routing_write_fire():
+    def update_routing_to_reg_inputs():
+      for i in range(num_tile_outports):
+        s.routing_crossbar.send_data[i].rdy @= s.send_data[i].rdy
       for i in range(num_fu_inports):
+        is_reg_write = \
+            s.ctrl_mem.send_ctrl.msg.write_reg_from[i] == PORT_ROUTING_CROSSBAR
+        s.register_cluster.recv_data_from_routing_crossbar[i].val @= \
+            s.routing_crossbar.send_data[num_tile_outports + i].val & \
+            ~is_reg_write
         s.routing_write_fire[i] @= \
             s.ctrl_mem.send_ctrl.val & \
-            (s.ctrl_mem.send_ctrl.msg.write_reg_from[i] == PORT_ROUTING_CROSSBAR) & \
+            is_reg_write & \
             s.routing_crossbar.send_data[num_tile_outports + i].val & \
             s.routing_crossbar.send_data[num_tile_outports + i].rdy
+        s.routing_crossbar.send_data[num_tile_outports + i].rdy @= \
+            is_reg_write | \
+            s.register_cluster.recv_data_from_routing_crossbar[i].rdy
+
+    @update
+    def update_reg_cluster_input_rdy():
+      for i in range(num_tile_outports):
+        s.fu_crossbar.send_data[i].rdy @= s.send_data[i].rdy
+      for i in range(num_fu_inports):
+        s.fu_crossbar.send_data[num_tile_outports + i].rdy @= \
+            s.register_cluster.recv_data_from_fu_crossbar[i].rdy
 
     # Clear ports are only useful during context switching.
     # We connect to 0 to make sure they have drivers.
